8cae42d48a2e0c5a246beffc79f13c7350d6b622
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-09 23:58:19 +0530

    Fluent Validation Using AbstractValidator

---
+++ StudentManagementCQRS/Features/Students/Commands/CreateStudent/CreateStudentCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace StudentManagementCQRS.Features.Students.Commands.CreateStudent
+{
+    public class CreateStudentCommandValidator : 
+        AbstractValidator<CreateStudentCommand>
+    {
+
+        public CreateStudentCommandValidator()
+        {
+            RuleFor(x => x.Name)
+               .NotEmpty()
+               .MaximumLength(100);
+
+            RuleFor(x => x.Age)
+                .InclusiveBetween(18, 60);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Department)
+                .NotEmpty();
+        }
+    }
+}
