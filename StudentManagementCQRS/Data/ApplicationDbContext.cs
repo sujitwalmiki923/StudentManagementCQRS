58cbd774218ba83f84fb6459f071578d10ca54ff
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-09 17:26:16 +0530

    Add project files.

---
+++ StudentManagementCQRS/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementCQRS.Entities;
+
+namespace StudentManagementCQRS.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentManagementCQRS/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentManagementCQRS.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Department { get; set; } = string.Empty;
+
+    }
+}
